b1954d6d437238ff12df80dd900a100f52c6e467
jaxnalia <jacksendall@gmail.com>
2026-02-22 04:18:53 +0000

    ssl update for db

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('https://pocketbase-o0w0kggwk8gg0c0oog800w0w.141.147.92.219.sslip.io/');
+export const pb = new PocketBase('https://db.pulsechain.wtf/');
 
