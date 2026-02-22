93e0bd165c8b3f6de1af223cc1d4d2e8f01fb832
jaxnalia <jacksendall@gmail.com>
2026-02-22 03:22:08 +0000

    url fix for prod

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,3 +1,4 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://pocketbase-o0w0kggwk8gg0c0oog800w0w.141.147.92.219.sslip.io/');
+export const pb = new PocketBase('https://pocketbase-o0w0kggwk8gg0c0oog800w0w.141.147.92.219.sslip.io/');
+
